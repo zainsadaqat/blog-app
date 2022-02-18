b384d553e98a6462de02cb138522abffe06d89c1
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-18 21:25:59 +0500

    Write test cases for posts_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: "users#index", as: 'users'
   get "/users/:id", to: "users#show", as: 'user'
 
-  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts", to: "posts#index", as: 'posts'
   get "/users/:id/posts/:id", to: "posts#show"
 end
---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "Inside posts_controller:", :type => :request do
+  describe 'GET /users/:id/posts' do
+    before(:example) { get('/users/12/posts') }
+
+    it 'server return 200 server ok' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'Rendering Index Template' do
+      expect(response).to render_template(:index)
+    end
+
+    it "Body Content of Index.html.erb" do
+      expect(response.body).to include('<h1>posts</h1>')
+    end
+  end
+
+  describe 'GET /users/:id' do
+    before(:example) { get('/users/12/posts/1') }
+
+    it 'return 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Rendering Show Template' do
+      expect(response).to render_template(:show)
+    end
+
+    it "Body Content of Show.html.erb" do
+      expect(response.body).to include('<h1>posts</h1>')
+    end    
+  end
+end
