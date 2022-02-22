301ce1644ad3adf0b80987a9d2c8d417d1212f40
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-23 02:54:06 +0500

    Add methods in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.length)
+  end
+
+  def recent_comments
+    Comment.order(id: :desc).take(5)
+  end
 end
