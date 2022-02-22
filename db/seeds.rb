a32f5e6cb17be7d1e07dd4de6acc5ad7bd77f3cf
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-22 22:54:14 +0500

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+third_user = User.create(name: 'Zain', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Pakistan.')
+fourth_user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Canada.')
+
+first_post = Post.create(title: 'Hello1', text: 'This is my first post1', user_id: first_user.id)
+second_post = Post.create(title: 'Hello2', text: 'This is my first post2', user_id: second_user.id)
+
+Comment.create(post: first_post.id, author: first_user.id, text: 'Hi Tom!1')
+Comment.create(post: second_post.id, author: second_user.id, text: 'Hi Tom!2')
