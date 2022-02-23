e17bd3f938418bf93aff65bb0c246ea325ab2a36
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-23 05:41:36 +0500

    Add User association in all models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :post
-  belongs_to :user
+  belongs_to :user, class_name: 'User'
 
   def update_likes_counter
     Post.update(likes_counter: post.likes.length)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: "User"
   has_many :likes
   has_many :comments
 
