e8bae1c92ebd8989adc29fadea1fd0c5d92b6d0f
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-25 23:42:03 +0500

    Add route for posts new action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show", as: 'user'
 
   get "/users/:id/posts", to: "posts#index", as: 'posts'
+  get "/users/:id/posts/new", to: "posts#new", as: 'new_post'
   get "/users/:id/posts/:id", to: "posts#show", as: 'post'
+
+
+  # resources :users, only: [:index, :show] do
+  #   resources :posts, only: [:index, :show]
+  # end
 end
