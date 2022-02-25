d71f4e31b26c787fb25cc292e651fd2e64b2d7d5
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-25 23:43:31 +0500

    Add new action controller for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,13 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
+  def new
+    new_post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { new_post: new_post } }
+    end
+  end
+
   def show
     @user = User.find_by(id: params[:id])
     @post = Post.find_by(id: params[:id])
