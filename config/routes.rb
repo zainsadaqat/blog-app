57b76cf6a46bdb08f5a38a9e4b3a7c6deb240594
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-26 19:46:36 +0500

    Create routes for creating Posts, Comments and Likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root to: "users#index", as: 'users'
-  get "/users/:id", to: "users#show", as: 'user'
+  get "/users/:user_id", to: "users#show", as: 'user'
 
-  get "/users/:id/posts", to: "posts#index", as: 'posts'
-  get "/users/:id/posts/new", to: "posts#new", as: 'new_post'
-  get "/users/:id/posts/:id", to: "posts#show", as: 'post'
+  get "/users/:user_id/posts", to: "posts#index", as: 'posts'
+  get "/users/:user_id/posts/new", to: "posts#new", as: 'new_post'
+  post "/users/:user_id/posts", to: "posts#create"
+  get "/users/:user_id/posts/:post_id", to: "posts#show", as: 'post'
+  
+  get "/users/:user_id/posts/:post_id/comments", to: "comments#index", as: 'comments'
+  get "/users/:user_id/posts/:post_id/comments/new", to: "comments#new", as: 'new_comment'
+  post "/users/:user_id/posts/:post_id/comments", to: "comments#create"
 
-
-  # resources :users, only: [:index, :show] do
-  #   resources :posts, only: [:index, :show]
-  # end
+  post "/users/:user_id/posts/:post_id/likes", to: "likes#create", as: 'likes'
 end
