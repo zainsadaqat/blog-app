ad591119031da74ac0e40415cb3565f278157b04
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-26 19:47:37 +0500

    Create a comments_controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+
+  def index
+    @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
+  end
+
+  def new
+    @comment = Comment.new
+    respond_to do |format|
+      format.html { render :new, locals: { comment: @comment } }
+    end
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(user: @user, post: @post, text: params[:comment][:text])
+    if @comment.save
+      redirect_to comments_path
+    else
+      render 'new', locals: {comment: @comment}
+    end
+  end
+
+  def show
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def update_comments_counter
-    Post.update(comments_counter: post.comments.length)
+    post.update(comments_counter: post.comments.length)
   end
 end
