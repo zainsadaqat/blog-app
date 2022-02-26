fffe4e2d452c6bb3ba74b4086621b30b8f8f53c9
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-26 19:49:11 +0500

    Update posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,30 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: params[:id])
-    @posts = @user.posts
+    @user = User.find(params[:user_id])
   end
 
   def new
-    new_post = Post.new
+    post = Post.new
     respond_to do |format|
-      format.html { render :new, locals: { new_post: new_post } }
+      format.html { render :new, locals: { post: post } }
+    end
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.new(user: @user, title: params[:post][:title], text: params[:post][:text])
+    if @post.save
+      flash[:success] = "Post saved successfully"
+      redirect_to posts_path
+    else
+      flash.now[:error] = "Post was not saved ..."
+      render 'new', locals: { post: @post }
     end
   end
 
   def show
-    @user = User.find_by(id: params[:id])
-    @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: params[:user_id])
+    @post = Post.find_by(id: params[:post_id])
+    @like = Like.new
   end
 end
