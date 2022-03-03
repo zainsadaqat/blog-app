8a83f081f1e2ee51f43590131fcf5728883d31e4
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-04 02:37:12 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "users#index", as: 'root'
 
-  # Defines the root path route ("/")
-  root to: "users#index"
-  get "/users/:id", to: "users#show"
+  devise_for :users do 
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
 
-  get "/users/:id/posts", to: "posts#index"
-  get "/users/:id/posts/:id", to: "posts#show"
+  get "/users/:user_id", to: "users#show", as: 'user'
+
+  get "/users/:user_id/posts", to: "posts#index", as: 'posts'
+  get "/users/:user_id/posts/new", to: "posts#new", as: 'new_post'
+  post "/users/:user_id/posts", to: "posts#create"
+  get "/users/:user_id/posts/:post_id", to: "posts#show", as: 'post'
+  
+  get "/users/:user_id/posts/:post_id/comments", to: "comments#index", as: 'comments'
+  get "/users/:user_id/posts/:post_id/comments/new", to: "comments#new", as: 'new_comment'
+  post "/users/:user_id/posts/:post_id/comments", to: "comments#create"
+
+  post "/users/:user_id/posts/:post_id/likes", to: "likes#create", as: 'likes'
 end
