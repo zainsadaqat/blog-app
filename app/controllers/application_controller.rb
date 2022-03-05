e05811e84dae6cf46c988ea756c09f4cba000fd9
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-05 06:03:37 +0500

    Add permission for role column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio role email password password_confirmation])
   end
 end
